b5a9f49a537c8b69898b5f1743f81cd3a2a29834
Jozze <jozze.zepl@hotmail.com>
2013-07-04 19:51:19 +0200

    Backup routine

--- baraction.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-conky -c /home/jozze/conkyFreeBSD.conf | while read LINE; do xsetroot -name "$LINE"; done
--- initscreen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-xcompmgr -cCfF -D 7 -r 15 &
-feh --bg-fill /home/jozze/backgrounds/Intension_by_vhm_alex.jpg &
-
